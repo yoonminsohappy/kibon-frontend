de3a5c284db6dba3ac13aeea09a4a2a603c17d9a
김건우 <gunoo0126@gmail.com>
2020-08-27 03:12:45 +0900

    Add: 검색결과 창 레이아웃 - 검색 결과창 기획 및 레이아웃 구현

--- src/Components/Nav/Search/Search.js
+++ src/Components/Nav/Search/Search.js
@@ -19,13 +19,67 @@ class Search extends Component {
           <div className="container">
             <div className="searchForm">
               <Link to="">브랜드 전체</Link>
-              <input className="searchText" onChange={this.searchText}></input>
+              <input
+                type="text"
+                className="searchText"
+                placeholder="해장"
+                onChange={this.searchText}
+              ></input>
               <img
                 src="./Images/btn_search.png"
                 onClick={() =>
                   this.props.history.push(`/${this.state.searchText}`)
                 }
               />
+            </div>
+            <div className="keywordBox">
+              <div className="todayBlock">
+                <dl>
+                  <dt>오늘의 키워드</dt>
+                  <dd>
+                    <Link to="#" className="sharp strong">
+                      선물
+                    </Link>
+                    <Link to="#" className="sharp">
+                      해장
+                    </Link>
+                    <Link to="#" className="sharp">
+                      원기회복
+                    </Link>
+                    <Link to="#" className="sharp strong">
+                      보양식
+                    </Link>
+                    <Link to="#" className="sharp">
+                      다이어트
+                    </Link>
+                    <Link to="#" className="sharp">
+                      술안주
+                    </Link>
+                  </dd>
+                </dl>
+              </div>
+              <div className="keyWord">
+                <dl>
+                  <dt>본 인기메뉴</dt>
+                  <dd>
+                    <Link to="#" className="sharp strong">
+                      해물김치해장죽
+                    </Link>
+                    <Link to="#" className="sharp">
+                      쇠고기야채죽
+                    </Link>
+                    <Link to="#" className="sharp">
+                      광양식바싹불고기 반상
+                    </Link>
+                    <Link to="#" className="sharp strong">
+                      백설
+                    </Link>
+                    <Link to="#" className="sharp">
+                      바싹불고기오징어 한상
+                    </Link>
+                  </dd>
+                </dl>
+              </div>
             </div>
           </div>
         </div>
--- src/Pages/SearchResult/SearchResult.js
+++ src/Pages/SearchResult/SearchResult.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import {Link} from "react-router-dom";
+import Nav from "../../Components/Nav/Nav"
+import Footer from "../../Components/Footer/Footer"
 import "./SearchResult.scss";
 
 class SearchResult extends Component {
@@ -20,14 +23,41 @@ class SearchResult extends Component {
   render() {
     console.log(this.state.menuList) 
     return (
-      <div>
-        <div>내 검색어: {this.props.match.params.text}</div>
-        {this.state.menuList && this.state.menuList.map((v,idx) => {
-          return <div style={{textAlign: "center"}}>{v.name}, {parseInt(v.price)}</div>;
-        })}
+      <div className="SearchResult">
+        <div className="topBox" />
+        <Nav />
+        <h2>
+          <strong>'{this.props.match.params.text}'</strong>
+          검색결과
+        </h2>
+        <div className="boundary"></div>
+        <div className="innerContents">
+          <div className="popularList">
+            <h3>본 인기메뉴</h3>
+            {POPULAR_MENU.map((v)=>{
+              return(
+              <Link to="#" key={v}>{v}</Link>
+              )
+            })}
+
+          </div>
+        </div>
+        {this.state.menuList &&
+          this.state.menuList.map((v, idx) => {
+            return (
+              <div style={{ textAlign: "center" }}>
+                {v.name}, {parseInt(v.price)}
+              </div>
+            );
+          })}
+        {/* <Footer /> */}
       </div>
     );
   }
 }
  
 export default SearchResult
+
+const POPULAR_MENU=[
+  "해물김치해장죽", "쇠고기야채죽", "광양식바싹불고기 반상", "백설", "바싹불고기오징어 한상"
+]
